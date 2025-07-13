4baf855233598f023681f475bb9bae088310d3ab
Ribbon-c <Tanzanite.tlq@gmail.com>
2025-07-14 00:08:01 +0900

    homework

--- day05_homework.R
+++ day05_homework.R
@@ -7,12 +7,19 @@
 
 # Before doing anything else, load the packages you need for this assignment:
 # `tidyverse` and `nycflights13`
+install.packages("nycflights13")
+
+library(tidyverse)
+library(nycflights13)
 
 # Q1. The `nycflights13` package includes a dataframe called `planes` that
 # contains data for each airplane such as manufacturer, model, etc.
 #
 # Join the `planes` dataframe to the `flights` dataframe, keeping all rows in
 # `flights`. Save the result as answer_1
+
+answer_1 <- left_join(flights, planes, by = "tailnum")
+
 #
 # HINT: the `tailnum` column uniquely identifies each airplane.
 
@@ -22,10 +29,17 @@
 #
 # HINT: use `count()`
 
+answer_2 <- answer_1 |>
+  count(manufacturer, sort = TRUE)
+
+
 # Q3. Does every flight in the `flights` dataframe have a corresponding
 # airplane in `planes`?
 #
 # Save your answer as a logical vector (either TRUE or FALSE) to answer_3
+
+answer_3 <- all(!is.na(answer_1$manufacturer))
+
 
 # Q4. The `nycflights13` package includes a dataframe called `airlines` that
 # includes the name of each airline and its corresponding carrier code.
@@ -34,9 +48,16 @@
 # Join the `airlines` dataframe to the `flights` dataframe, keeping all rows in
 # `flights`. Save the result as answer_4
 
+answer_4 <- left_join(flights, airlines, by = "carrier")
+
 # Q5. Use answer_4 to find the average arrival delay for each airline.
 # Exclude missing values from your calculation.
 # Save the result as answer_5
+
+answer_5 <- answer_4 |>
+  group_by(name) |>
+  summarize(avg_arr_delay = mean(arr_delay, na.rm = TRUE))
+
 #
 # HINT: remember that you can use `group_by()` and `summarize()` together to
 # calculate summary statistics for each group in the data.
